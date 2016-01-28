f0320aaa423e65ff969aa9a7c517bff8df6e10a9
ericmguimaraes <eric@popcode.com.br>
2016-01-28 15:59:23 -0300

    Basic Structure SableCC with a simple language

---
+++ CminusCompiler/src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+  
+import java.io.FileReader;
+import java.io.PushbackReader;
+
+import cminus.interpret.Interpreter;
+import cminus.lexer.Lexer;
+import cminus.node.Start;
+import cminus.parser.Parser; 
+  
+public class Main { 
+   public static void main(String[] args) { 
+      if (args.length > 0) { 
+         try { 
+            /* Form our AST */ 
+            Lexer lexer = new Lexer (new PushbackReader( 
+               new FileReader(args[0]), 1024)); 
+            Parser parser = new Parser(lexer); 
+            Start ast = parser.parse() ; 
+  
+            /* Get our Interpreter going. */ 
+            Interpreter interp = new Interpreter () ; 
+            ast.apply(interp) ; 
+         } 
+         catch (Exception e) { 
+            System.out.println (e) ; 
+         } 
+      } else { 
+         System.err.println("usage: java simpleAdder inputFile"); 
+         System.exit(1); 
+      } 
+   } 
+}
