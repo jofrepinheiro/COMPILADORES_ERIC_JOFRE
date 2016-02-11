d60d1c093f29eac1e5f2a5272022a44e72620e19
jofrepinheiro <jofrepinheiro@gmail.com>
2016-02-11 14:09:57 -0300

    Impressão dos Tokens em Ordem

--- CminusCompiler/src/main/Main.java
+++ CminusCompiler/src/main/Main.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.PushbackReader;
 
 import cminus.node.Start;
+import cminus.node.Token;
 import cminus.parser.Parser; 
   
 public class Main { 
@@ -14,16 +15,36 @@ public class Main {
             /* Form our AST */ 
         	 MyLexer lexer = new MyLexer (new PushbackReader( 
                new FileReader(args[0]), 1024));
-        	 while(!lexer.peek().getText().equals("")){ 
-        		 System.out.println(lexer.peek().getClass()); 
-        	     lexer.next(); 
-        	} //Bugado, Consertar.
+//        	 while(!lexer.peek().getText().equals("")){ 
+//        		 System.out.println(lexer.peek().getClass()); 
+//        	     lexer.next(); 
+//        	} //Bugado, Consertar.
+        	 
+        	 
+        	 // Ainda bugado
+        	
+        	 while (true) {
+             	Token t = lexer.next();
+     		 String newline = t.toString();
+             	String identificado = t.getClass().toString().replaceAll("class node\\.T?", "");
+             	if(!identificado.equals("EOF")){
+             		if (newline.contains("\n"))
+             			System.out.println(identificado);
+             		else
+             			System.out.print(identificado + " ");
+             	}		
+             	if(t.getText().isEmpty()){
+             		System.out.println("\n");
+             		break;
+             	}
+         	} 
+        	
             Parser parser = new Parser(lexer); 
             Start ast = parser.parse() ; 
             	
             /* Get our Interpreter going. */ 
             Interpreter interp = new Interpreter () ; 
-            ast.apply(interp) ; 
+            ast.apply(interp) ;
          } 
          catch (Exception e) { 
             throw e;
