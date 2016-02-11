dbdf4225bb530e8a3a29b7574662243ed3fa5a52
jofrepinheiro <jofrepinheiro@gmail.com>
2016-02-11 15:43:39 -0300

    Adição da impressão para arquivo de tokens

--- CminusCompiler/src/main/Main.java
+++ CminusCompiler/src/main/Main.java
@@ -1,57 +1,61 @@
 package main;
 
-  
+
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.io.PushbackReader;
 
 import cminus.node.Start;
 import cminus.node.Token;
 import cminus.parser.Parser; 
-  
+
 public class Main { 
-   public static void main(String[] args) throws Exception { 
-      if (args.length > 0) { 
-         try { 
-            /* Form our AST */ 
-        	 MyLexer lexer = new MyLexer (new PushbackReader( 
-               new FileReader(args[0]), 1024));
-//        	 while(!lexer.peek().getText().equals("")){ 
-//        		 System.out.println(lexer.peek().getClass()); 
-//        	     lexer.next(); 
-//        	} //Bugado, Consertar.
-        	 
-        	 
-        	 // Ainda bugado
-        	
-        	 while (true) {
-             	Token t = lexer.next();
-     		 String newline = t.toString();
-             	String identificado = t.getClass().toString().replaceAll("class node\\.T?", "");
-             	if(!identificado.equals("EOF")){
-             		if (newline.contains("\n"))
-             			System.out.println(identificado);
-             		else
-             			System.out.print(identificado + " ");
-             	}		
-             	if(t.getText().isEmpty()){
-             		System.out.println("\n");
-             		break;
-             	}
-         	} 
-        	
-            Parser parser = new Parser(lexer); 
-            Start ast = parser.parse() ; 
-            	
-            /* Get our Interpreter going. */ 
-            Interpreter interp = new Interpreter () ; 
-            ast.apply(interp) ;
-         } 
-         catch (Exception e) { 
-            throw e;
-         } 
-      } else { 
-         System.err.println("usage: java cminus inputFile"); 
-         System.exit(1); 
-      } 
-   } 
+	public static void main(String[] args) throws Exception { 
+		if (args.length > 0) { 
+			try { 
+				/* Form our AST */ 
+				MyLexer lexer = new MyLexer (new PushbackReader( 
+						new FileReader(args[0]), 1024));
+
+				String tokenOutput = "";
+				while (true) {
+					Token t = lexer.next();
+					String newline = t.toString();
+					String token = t.getClass().toString().replaceAll("class node\\.T?", "").replace("class cminus.node.","");
+					if(!token.equals("EOF")){
+						if (newline.contains("\n"))
+							tokenOutput+="["+token+"]\n";
+						else
+							tokenOutput+='['+token+']' + " ";
+					}		
+					if(t.getText().isEmpty()){
+						tokenOutput+=("\n\n");
+						break;
+					}
+				}
+				//Interpreter Test, TBD
+				//            Parser parser = new Parser(lexer); 
+				//            Start ast = parser.parse() ; 
+				//            	
+				//            /* Get our Interpreter going. */ 
+				//            Interpreter interp = new Interpreter () ; 
+				//            ast.apply(interp) ;
+				
+				//Write to Tokens File
+				try(PrintWriter tokenFile = new PrintWriter("testes/TokenOutput.txt")) {
+					tokenFile.println(tokenOutput);
+					tokenFile.close();
+					System.out.println("Finished. Check your tokens file.");
+				} catch (Exception e) {
+					System.out.println("File not Found");
+				}
+			} 
+			catch (Exception e) { 
+				throw e;
+			} 
+		} else { 
+			System.err.println("usage: java cminus inputFile"); 
+			System.exit(1); 
+		} 
+	} 
 }
