7ddc9ca3feeae9f379622569ac6fb8fb29a0b318
ericmguimaraes <eric@popcode.com.br>
2016-03-17 11:46:21 -0300

    tudo do commit anterior que eu esqueci de adicionar :)

--- CminusCompiler/src/main/Main.java
+++ CminusCompiler/src/main/Main.java
@@ -1,61 +1,66 @@
 package main;
 
-
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.PushbackReader;
 
+import cminus.lexer.Lexer;
+import cminus.lexer.LexerException;
+import cminus.node.PProgram;
 import cminus.node.Start;
 import cminus.node.Token;
-import cminus.parser.Parser; 
+import cminus.parser.Parser;
 
-public class Main { 
-	public static void main(String[] args) throws Exception { 
-		if (args.length > 0) { 
-			try { 
-				/* Form our AST */ 
-				MyLexer lexer = new MyLexer (new PushbackReader( 
-						new FileReader(args[0]), 1024));
+public class Main {
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0) {
+				/* Form our AST */
+				MyLexer lexer = new MyLexer(new PushbackReader(new FileReader(args[0]), 1024));
+				printTokens(lexer);
+				Parser parser = new Parser(lexer);
+				Start start = parser.parse();
+		} else {
+			System.err.println("usage: java cminus inputFile");
+			System.exit(1);
+		}
+	}
 
-				String tokenOutput = "";
-				while (true) {
-					Token t = lexer.next();
-					String newline = t.toString();
-					String token = t.getClass().toString().replaceAll("class node\\.T?", "").replace("class cminus.node.","");
-					if(!token.equals("EOF")){
-						if (newline.contains("\n"))
-							tokenOutput+="["+token+"]\n";
-						else
-							tokenOutput+='['+token+']' + " ";
-					}		
-					if(t.getText().isEmpty()){
-						tokenOutput+=("\n\n");
-						break;
-					}
+	public static void printTokens(Lexer lexer) {
+		String tokenOutput = "";
+		while (true) {
+			Token t;
+			try {
+				t = lexer.next();
+
+				String newline = t.toString();
+				String token = t.getClass().toString().replaceAll("class node\\.T?", "").replace("class cminus.node.",
+						"");
+				if (!token.equals("EOF")) {
+					if (newline.contains("\n"))
+						tokenOutput += "[" + token + "]\n";
+					else
+						tokenOutput += '[' + token + ']' + " ";
 				}
-				//Interpreter Test, TBD
-				//            Parser parser = new Parser(lexer); 
-				//            Start ast = parser.parse() ; 
-				//            	
-				//            /* Get our Interpreter going. */ 
-				//            Interpreter interp = new Interpreter () ; 
-				//            ast.apply(interp) ;
-				
-				//Write to Tokens File
-				try(PrintWriter tokenFile = new PrintWriter("testes/TokenOutput.txt")) {
-					tokenFile.println(tokenOutput);
-					tokenFile.close();
-					System.out.println("Finished. Check your tokens file.");
-				} catch (Exception e) {
-					System.out.println("File not Found");
+				if (t.getText().isEmpty()) {
+					tokenOutput += ("\n\n");
+					break;
 				}
-			} 
-			catch (Exception e) { 
-				throw e;
-			} 
-		} else { 
-			System.err.println("usage: java cminus inputFile"); 
-			System.exit(1); 
-		} 
-	} 
+			} catch (LexerException | IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+
+		// Write to Tokens File
+		try (PrintWriter tokenFile = new PrintWriter("testes/TokenOutput.txt")) {
+			tokenFile.println(tokenOutput);
+			tokenFile.close();
+			System.out.println("Finished. Check your tokens file.");
+		} catch (Exception e) {
+			System.out.println("File not Found");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 }
